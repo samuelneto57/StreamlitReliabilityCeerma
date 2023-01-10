62c493506f959fcf4cbb094974ef3e3dc55490ec
biasalesc <65680806+biasalesc@users.noreply.github.com>
2023-01-10 16:20:16 -0500

    Update index.py

--- index.py
+++ index.py
@@ -1,12 +1,8 @@
-# Import libraries
 import streamlit as st
+
+import show_parametricmodel , show_parametricmix, show_otherfunc,show_comingsoon, show_repairable, show_alt, show_fitter
 from PIL import Image
 
-import show_parametricmodel , show_parametricmix, show_otherfunc,\
-show_comingsoon, show_repairable, show_alt, show_fitter, show_reliabilitytest, \
-show_mixture, show_competingrisk
-
-# Page layout
 image_ufpe = Image.open('./src/logo.png')
 image_pip = Image.open('./src/logopip.png')
 
@@ -33,79 +29,60 @@ for reliability related analysis and visualization using the Reliability Python 
 """)
 st.sidebar.write("")
 
-# 👀 〽️ 🏃 🔧 🔍 💪
-# Module selection
 first_menu = st.sidebar.selectbox(
     "Which module do you want to use?",
-    ("Select a module","Distribution Visualization", "Fit Distribution to Data", 
-    "Accelerated life testing", "Repairable Systems"," Reliability Testing", 
-    "Stress and Strentgh")
+    ("Select a module","Parametrics and Non-Parametrics Models", "Accelerated life testing", "Repairable Systems", "Other Functions")
 )
 
-if first_menu == "Distribution Visualization":
+if first_menu == "Parametrics and Non-Parametrics Models":
     add_selectbox = st.sidebar.selectbox(
         "Which submodule do you want to use?",
-        ("Select a submodule", "Parametric Distributions", "Mixture Model","Competing Risk Model", "Non-Parametric Model")
+        ("Select a submodule", "Parametric Model", "Parametric Mix Model", "Non-Parametric model", "Fitter")
     )
 
     if add_selectbox == "Select a module":
         pass
-    if add_selectbox == "Parametric Distributions":
+    if add_selectbox == "Parametric Model":
         show_parametricmodel.show()
-    # if add_selectbox == "Parametric Mix Model":
-    #     show_parametricmix.show()
-    if add_selectbox == 'Mixture Model':
-        show_mixture.show()
-    if add_selectbox == 'Competing Risk Model':
-        show_competingrisk.show()
+    if add_selectbox == "Parametric Mix Model":
+        show_parametricmix.show()
     if add_selectbox == "Non-Parametric model":
         show_comingsoon.show()
-
-if first_menu == "Fit Distribution to Data":
-    show_fitter.show()
+    if add_selectbox == "Fitter":
+        show_fitter.show()
 
 
 if first_menu == "Accelerated life testing":
-    show_alt.show()
+    add_selectbox = st.sidebar.selectbox(
+        "Which submodule do you want to use?",
+        ("Select a submodule", "Accelerated life testing")
+    )
+
+    if add_selectbox == "Select a module":
+        pass
+    if add_selectbox == "Accelerated life testing":
+        show_alt.show()
 
 
 if first_menu == "Repairable Systems":
-    show_repairable.show()
+    add_selectbox = st.sidebar.selectbox(
+        "Which submodule do you want to use?",
+        ("Select a submodule", "Repairable Systems")
+    )
 
-        
-if first_menu == "Reliability Testing":
-    show_reliabilitytest.show()
+    if add_selectbox == "Select a module":
+        pass
+    if add_selectbox == "Repairable Systems":
+        show_repairable.show()
 
 
-if first_menu == "Stress and Strentgh":
-    show_otherfunc.show()
+if first_menu == "Other Functions":
+    add_selectbox = st.sidebar.selectbox(
+        "Which submodule do you want to use?",
+        ("Select a submodule", "Stress and Strentgh")
+    )
+    if add_selectbox == "Select a module":
+        pass
 
-# Authors name and link
-# authors_css = """
-#         style='
-#         display: block;
-#         margin-bottom: 0px;
-#         margin-top: 0px;
-#         padding-top: 0px;
-#         font-weight: 400;
-#         font-size:1.1em;
-#         filter: brightness(85%);
-#         text-align: center;
-#         text-decoration: none;
-#         '
-# """
-#color:#DBBD8A;
-
-# st.sidebar.markdown(
-#     '<p ' + authors_css + '>' + 'By </p>',
-#         unsafe_allow_html=True
-# )
-# st.sidebar.markdown(f"\n\n\n")
-# st.sidebar.markdown(
-#     '<a ' + authors_css + ' target="_blank" href="https://github.com/biasalesc">' + 'Beatriz Cunha</a>',
-#     unsafe_allow_html=True,
-# )
-# st.sidebar.markdown(
-#     '<a ' + authors_css + ' target="_blank" href="https://github.com/yop2yop">' + 'Diego</a>',
-#     unsafe_allow_html=True,
-# )
+    if add_selectbox == "Stress and Strentgh":
+        show_otherfunc.show()
